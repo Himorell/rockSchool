75797d0cda7f2f7b1e80e6168e263bc5c54d1889
Verónica Flores Gutiérrez <veflo13@gmail.com>
2022-12-09 13:22:44 +0100

    add bars and  actionCreate button

--- src/Views/Components/layout.php
+++ src/Views/Components/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="./public/styles/main.css">
     <link rel="shortcut icon" type="image/x-icon" href="../public/images/favicon.ico">
     <title>Zoombadass RockSchool</title>
 </head>
